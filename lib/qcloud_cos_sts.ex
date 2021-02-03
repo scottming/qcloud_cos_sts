4936603ae7e9fbf78e887988140150583e34e65a
scottming <therealscottming@gmail.com>
2021-02-03 21:26:09 +0800

    Fix a bug

--- lib/qcloud_cos_sts.ex
+++ lib/qcloud_cos_sts.ex
@@ -14,9 +14,30 @@ defmodule QcloudCosSts do
     host = @config[:host] || ""
     region = @config[:region] || "ap-beijing"
     duration_seconds = to_string(@config[:duration_seconds] || 1800)
-    policy = @config[:policy]
 
-    policy_str = QcloudCosSts.Util.to_param_string(policy)
+    policy = %{
+      "version" => "2.0",
+      "statement" => [
+        %{
+          "action" => [
+            "name/cos:PutObject",
+            "name/cos:PostObject"
+          ],
+          "effect" => "allow",
+          "principal" => %{"qcs" => ["*"]},
+          "resource" => [
+            "qcs::cos:" <>
+              @config[:region] <>
+              ":uid/" <>
+              @config[:app_id] <>
+              ":prefix//" <>
+              @config[:app_id] <>
+              "/" <> @config[:short_bucket_name] <> "/" <> (@config[:allow_prefix] || "*")
+          ]
+        }
+      ]
+    }
+
     action = "GetFederationToken"
     nonce = QcloudCosSts.Util.get_random(10000, 20000)
     timestamp = :os.system_time(:second) |> to_string()
@@ -31,13 +52,26 @@ defmodule QcloudCosSts do
       "Name" => "cos-sts-elixir",
       "Version" => "2018-08-13",
       "Region" => region,
-      "Policy" => URI.encode(policy_str)
+      "Policy" => policy |> Jason.encode!() |> URI.encode(),
+      "SignatureMethod" => "HmacSHA256"
     }
 
     params =
       params |> Map.put("Signature", QcloudCosSts.Util.get_signature(params, secret_key, method))
 
     encoded_params = URI.encode_query(params)
-    HTTPoison.post(host, encoded_params, proxy: proxy)
+    HTTPoison.post("https://#{host}", encoded_params, proxy: proxy) |> parse_response
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{status_code: status_code} = response})
+       when status_code in 200..399 do
+    {:ok,
+     %{
+       body: response.body,
+       headers: response.headers,
+       status_code: response.status_code,
+       request: response.request,
+       request_url: response.request_url
+     }}
   end
 end
