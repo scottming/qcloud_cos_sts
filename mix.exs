fce79ef5568983dd15536cb60d3fd7f2e01ece8f
scottming <therealscottming@gmail.com>
2021-02-04 10:56:42 +0800

    Add some code form publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule QcloudCosSts.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/scottming/qcloud_cos_sts"
+
   def project do
     [
       app: :qcloud_cos_sts,
+      description: "An Elixir implementation of the Qcloud COS temporary key.",
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        files: ~w(mix.exs lib LICENSE* README.md CHANGELOG.md),
+        licenses: ["MIT"],
+        maintainers: ["ScottMing"],
+        links: %{
+          "GitHub" => @github_url
+        }
+      ]
     ]
   end
 
@@ -24,6 +35,7 @@ defmodule QcloudCosSts.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.2"}
     ]
