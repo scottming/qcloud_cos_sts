dcde9fba0ddd183f19ff4b8c12fd2bde1617d2c6
scottming <therealscottming@gmail.com>
2021-02-04 10:33:30 +0800

    Fix some configs

--- examples/dummy/config/dev.exs
+++ examples/dummy/config/dev.exs
@@ -1,1 +1,7 @@
 use Mix.Config
+
+config :qcloud_cos_sts,
+  secret_id: System.get_env("qcloud_cos_secret_id"),
+  secret_key: System.get_env("qcloud_cos_secret_key"),
+  bucket: System.get_env("qcloud_cos_bucket")
+  # region: "ap-beijing",
